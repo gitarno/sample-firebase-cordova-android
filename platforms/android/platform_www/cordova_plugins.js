8beb268881ebd73a3c3b31a3209c1e668b94b711
Arno Roldao Junior <arno@advg.com.br>
2018-01-22 16:17:51 -0200

    limpando o projeto

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,19 +1,8 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-  {
-    "id": "cordova-plugin-firebase-analytics.FirebaseAnalytics",
-    "file": "plugins/cordova-plugin-firebase-analytics/www/FirebaseAnalytics.js",
-    "pluginId": "cordova-plugin-firebase-analytics",
-    "merges": [
-      "cordova.plugins.firebase.analytics"
-    ]
-  }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-support-google-services": "1.0.0",
-  "cordova-plugin-firebase-analytics": "0.12.1",
   "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
